d7e1ef1d144159946bcf42c45946fefafebcaa06
Kati Raavik <kati.raavik@tptlive.ee>
2023-02-09 14:17:30 +0200

    add shopping list

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,38 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import { each, text } from "svelte/internal";
+
+  let message = 'Tere!';
+  let items = [
+    {name: 'Piim', isDone: false}, 
+    {name: 'sai', isDone: false}, 
+    {name: 'leib', isDone: false},
+  ];
+  function add(){
+    if (message.trim() !== ''){
+      items = [
+        ...items,
+        {name: message, isDone: false},
+      ];
+    }
+    message = '';
+  }
+
+  function keyDown(evt){
+    if(evt.key === 'Enter'){
+      add();
+    }
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<input type="text" bind:value={message} on:keydown={keyDown}/>
+<button on:click={()=>add()}>Add</button>
+<h1>{message}</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<ul>
+  {#each items as item}
+  <li>
+    {item.name}
+    <input type="checkbox" bind:checked={item.isDone}>
+    </li>
+  {/each}
+</ul>
